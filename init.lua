4eeb11bcb08adfe111bf3ed7d34423293f7a8434
Billy S <bbrus@comcast.net>
2018-11-29 20:48:50 -0500

    Completed ice-generation. Added special chest textures

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
 local mp = minetest.get_modpath(minetest.get_current_modname())
 
+holidays = {}
+
+holidays.holidays = {
+   christmas = 1
+}
+
+holidays.holiday = holidays.holidays.christmas
+
 dofile(mp .. "/christmas.lua")
